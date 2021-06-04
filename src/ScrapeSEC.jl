bb0662356ea90aa96d158ae430b6638173cdd27d
tylerjthomas9 <tylerjthomas9@gmail.com>
2021-06-04 14:38:13 -0500

    added comma between exports

--- src/ScrapeSEC.jl
+++ src/ScrapeSEC.jl
@@ -1,13 +1,12 @@
 module ScrapeSEC
 
     # source files
-    using Base: Bool
-include("download_metadata.jl")
+    include("download_metadata.jl")
     include("download_filings.jl")
 
     export 
     # metadata functions
-    get_metadata
+    get_metadata,
 
     # filing downloaders
     get_quarterly_filings, download_filing
