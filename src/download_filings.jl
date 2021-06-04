45614f899b3e3dae01b4349d4d52e37cf73cd881
tylerjthomas9 <tylerjthomas9@gmail.com>
2021-06-04 13:48:29 -0500

    added ability to download filings based off year/q

--- src/download_filings.jl
+++ src/download_filings.jl
@@ -34,9 +34,83 @@ function download_filing(file_name::String, full_file::String)
     return
 end
 
+"""
+Creates an array of file paths of the metadata files
+
+Parameters
+----------
+time_periods::Vector{Tuple{Int64, Int64}})
+    - Vector of time periods (year, quarter) to get metadata files Vector{Tuple{year, quarter}}
+metadata_dest::String
+    - Directory where metadata is stored
+"""
+function get_metadata_files(time_periods::Vector{Tuple{Int64, Int64}}, metadata_dest::String)::Vector{String}
+
+    # get file paths for all time get_time_periods
+    file_paths = [edgar_metadata_destprefix * string(i[1])*"/QTR"*string(i[2])*"/master.zip" for i in time_periods]
+
+    return file_paths
+end
+
+
 
 """
 Get quarterly filings from https://www.sec.gov/Archives/
+
+Parameters
+----------
+start_year::Int
+    - first year to download filings
+end_year::Int
+    - last year to download filings
+quarters::Vector{Int}
+    - Quarters to download filings from
+dest::String
+    - destination folder for downloaded filings
+filing_types::Vector{String}
+    - types of filings to download (eg. ["10-K", "10-Q"])
+download_rate::Int
+    - Number of filings to download every second (limit=10)
+metadata_dest::String
+    - Directory to store metadata files
+skip_file::Bool
+    - If true, existing files will be skipped
+skip_metadata_file::Bool
+    - If true, existing metadata files will be skipped
+"""
+function get_quarterly_filings(start_year::Int, end_year::Int; quarters=[1,2,3,4]::Vector{Int}, 
+                                dest="../data/"::String; filing_types=["10-K", ]::Vector{String}, 
+                                download_rate=10::Int, metadata_dest="../metadata/"::String,
+                                skip_file=true::Bool, skip_metadata_file=false:Bool)
+
+                                # get current year, quarter to prevent errors trying to get future data
+    current_date = Dates.now()
+    current_year = Dates.year(current_date)
+    current_quarter = Dates.quarterofyear(current_date)
+    
+    # set end year to current year if no year is specified
+    if end_year == nothing
+        end_year = current_year
+    end
+
+    # get an array of dates to download metadata
+    years = collect(start_year:end_year)
+    time_periods = [(y, q) for y in years for q in quarters if (q <= current_quarter || y < current_year)]
+
+    # make sure all the metadata is downloaded
+    get_metadata(start_year, end_Year; quarters=quarters, download_rate=download_rate, dest=metadata_dest)
+
+    file_paths = [metadata_dest * string(t[1]) * "-QTR" * string(t[2]) * ".tsv"]
+
+    for file in file_paths
+        get_quarterly_filings(file; dest=dest; filing_types=filing_types, 
+                            download_rate=download_rate, skip_file=skip_file)
+    end
+
+end
+
+"""
+Get quarterly filings from https://www.sec.gov/Archives/ using a metadata file
 
 Parameters
 ----------
@@ -48,8 +122,11 @@ filing_types::Vector{String}
     - types of filings to download (eg. ["10-K", "10-Q"])
 download_rate::Int
     - Number of filings to download every second (limit=10)
+skip_file::Bool
+    - If true, existing files will be skipped
 """
-function get_quarterly_filings(metadata_file::String, dest="../data/"::String; filing_types=["10-K", ]::Vector{String}, download_rate=10::Int)
+function get_quarterly_filings(metadata_file::String; dest="../data/"::String; filing_types=["10-K", ]::Vector{String}, 
+                            download_rate=10::Int, skip_file=true::Bool)
 
     # verify download_rate is valid (less than 10 requests per second, more than 0)
     if download_rate > 10
@@ -59,8 +136,7 @@ function get_quarterly_filings(metadata_file::String, dest="../data/"::String; f
         download_rate = 1
         println("download_rate of less than 1 per second(", download_rate, ") is not valid. download_rate has been set to 1/second.")
     end
-
-
+    
     println("Metadata: " * metadata_file)
     
     df = DataFrame(CSV.File(metadata_file, delim="|"))
