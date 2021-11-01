b386133dc37500838e8c1738416cde7e88454e9e
tylerjthomas9 <tylerjthomas9@gmail.com>
2021-10-31 19:53:39 -0500

    fix test for download_metadata

--- test/download_filings.jl
+++ test/download_filings.jl
@@ -4,7 +4,7 @@ using Test
 @testset "download_filing() (Download Filing)" begin
     
     file_name = "edgar/data/880794/9999999997-05-050434.txt"
-    full_file = "temp_filing.txt"
+    full_file = "./temp_filing.txt"
     dest = "test/"
     download_filing(file_name, full_file, dest)
     @test isfile(full_file)
