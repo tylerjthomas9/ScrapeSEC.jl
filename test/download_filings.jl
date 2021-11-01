628c3321e1ffd591d8e5c04f464a113b9fc9f552
tylerjthomas9 <tylerjthomas9@gmail.com>
2021-10-31 19:58:11 -0500

    update download_filing test

--- test/download_filings.jl
+++ test/download_filings.jl
@@ -3,9 +3,13 @@ using Test
 
 @testset "download_filing() (Download Filing)" begin
     
+
     file_name = "edgar/data/880794/9999999997-05-050434.txt"
     full_file = "./temp_filing.txt"
     dest = "test/"
+    if !isdir(dest)
+        mkdir(dest)
+    end
     download_filing(file_name, full_file, dest)
     @test isfile(full_file)
     rm(full_file)
