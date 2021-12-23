5177208bd74f0d3929179c301780a6b5dc226fe4
tylerjthomas9 <tylerjthomas9@gmail.com>
2021-12-22 22:59:12 -0600

    add default julia .gitignore files

--- test/download_metadata.jl
+++ test/download_metadata.jl
@@ -1,7 +1,21 @@
-using ScrapeSEC
-using Test
 
-@testset "" begin
+@testset "get_metadata_urls()" begin
+    time_periods = [(2020,1), (2020, 2), (2021, 1), (2021, 2)]
+    urls = ["https://www.sec.gov/Archives/edgar/full-index/2020/QTR1/master.zip",
+            "https://www.sec.gov/Archives/edgar/full-index/2020/QTR2/master.zip",
+            "https://www.sec.gov/Archives/edgar/full-index/2021/QTR1/master.zip",
+            "https://www.sec.gov/Archives/edgar/full-index/2021/QTR2/master.zip"]
+    @test urls == get_metadata_urls(time_periods)
+
+    inferred_type = @inferred get_metadata_urls(time_periods)
+    @test inferred_type == Vector{String}
+end
+
+@testset "download_metadata()" begin
+
+    url = "https://www.sec.gov/Archives/edgar/full-index/2020/QTR1/master.zip"
+    dest = "./"
+    temp_file = "temp_metadata"
+    download_metadata(url, dest, temp_File)
     
-
 end
